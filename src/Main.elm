d8d6d3ff7add0bdfb395d2e9955261c3de0cb76b
seito2 <seito1201@gmial.com>
2019-07-08 19:49:25 +0900

    update

--- src/Main.elm
+++ src/Main.elm
@@ -51,13 +51,29 @@ update msg model =
 
 view : Model -> Browser.Document Msg
 view model =
-    { title = "Elm 0.19 starter"
+    { title = "telstar-freshman"
     , body =
         [ siteHeader
         , homeGif
         , title "01" "Our Vision" "団体理念"
         , detail "宇宙産業を日本の基幹産業に" "私たちTELSTARは、宇宙産業を日本の基幹産業にすることを理念に掲げています。近い未来、宇宙産業は私たちの社会に欠かせないものになると考えています。中高生の”宇宙は難しい”という意識を興味へと変えることで、来たる宇宙時代、日本が宇宙産業で世界をリードしていく未来を目指しています。"
-        , siteFooter
+        , title "02" "Activity Purpose" "活動目的"
+        , detail "「 宇宙への興味を０→１へ 」" " 私たちTELSTARは、宇宙をフィールドに活躍する多種多様な人材を創出するため、宇宙への興味の入り口を提供する活動を行っています。"
+        , detail "" "進路選択の重要な時期である中高生をメインターゲットとして、宇宙フリーマガジンTELSTARの発行を中心に、ウェブサイト・SNSによる情報発信やイベントなどの広報活動を推進しています"
+        , title "03" "Content" "活動内容"
+        , detail "FREE MAGAZINE" "宇宙フリーマガジン「TELSTAR」は、大学生の視点から宇宙を伝える、今までになかった宇宙雑誌です。研究機関や企業を取材した記事を美大生を中心に分かりやすいデザインでまとめ、文理を問わずに宇宙の魅力を紹介しています。完成したフリーマガジンは、全国の高校・科学館に配布され、宇宙教育の促進を目指しています。"
+        , img [ class "three-section" , src "./assets/index/telstar_freemagazine.jpg" ] []
+        , detail "EVENT" "フリーマガジンよりも幅広いターゲット層にアプローチできるイベント運営を行います。宇宙や教育に関する様々な外部イベントへのブース出展や、オリジナルで考えたコンセプトに沿った宇宙イベントの主催を行っています。気軽に参加でき、また私たちが参加者と直接関わることで興味を広げるサポートができるよう運営しています。"
+        , img [ class "three-section" , src "./assets/index/event.jpg" ] []
+        , detail "TELSTAR WEB" "TELSTARのWEBサイトでは、フリーマガジンに載せきれなかったインタビューなどの冊子スピンオフの他に、WEB特集や月や星まつわる話題を扱う記事なども掲載しています。また、TELSTARが参加もしくは主催する各種イベントの告知や報告の記事も随時更新しています。"
+        , img [ class "three-section" , src "./assets/index/web.png" ] []
+        , title "04" "Member" "メンバー紹介"
+        , img [ class "icon" , src "./assets/index/more_icon.jpg" ] []
+        , img [ class "japan-img" , src "./assets/index/japan.png" ] []
+        , title "05" "Position" "担当紹介"
+        -- , joinUs
+        -- , 
+        -- , siteFooter
         ]
     }
 
@@ -96,12 +112,9 @@ homeGif =
     div []
         [ div [ class "home-gif-container"]
             [ img [ class "top-background" , src "./assets/index/home.gif" ] [] ]
-        , img [ class "telstar-logo-on-background" , src "./assets/telstar_logo_white.png" ] []
+        , img [ class "telstar-logo-on-background" , src "./assets/index/telstar_logo_white.png" ] []
         ]
 
-siteFooter : Html Msg
-siteFooter =
-    footer [ class "site-footer" ]
-        [ p [ class "copyright" ] [ text "© 2019 y047aka" ]
-        ]
-
+joinUs : Html Msg
+joinUs = 
+    div [] []
